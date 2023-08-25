4d630051f1071d95348b62803173980391b9f4eb
Sean Liao <sean+git@liao.dev>
2023-08-25 22:59:27 +0100

    refresh

--- go.mod
+++ go.mod
@@ -1,20 +1,21 @@
 module go.seankhliao.com/vanity
 
-go 1.21
+go 1.21.0
 
 require (
-	github.com/google/subcommands v1.2.0
-	go.seankhliao.com/svcrunner/v2 v2.0.0-20230625191511-8a3178bf3d11
+	go.seankhliao.com/svcrunner/v3 v3.0.0-20230825210239-4f5a0684e6c9
 	go.seankhliao.com/webstyle v0.0.0-20230625120550-9571efcb99d4
 )
 
 require (
 	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.2 // indirect
 	github.com/yuin/goldmark v1.5.4 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.42.0 // indirect
 	go.opentelemetry.io/otel v1.16.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.16.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlpmetric v0.39.0 // indirect
@@ -26,11 +27,11 @@ require (
 	go.opentelemetry.io/otel/sdk/metric v0.39.0 // indirect
 	go.opentelemetry.io/otel/trace v1.16.0 // indirect
 	go.opentelemetry.io/proto/otlp v0.20.0 // indirect
-	golang.org/x/net v0.11.0 // indirect
-	golang.org/x/sys v0.9.0 // indirect
-	golang.org/x/text v0.10.0 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20230530153820-e85fd2cbaebc // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
-	google.golang.org/grpc v1.56.1 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/grpc v1.57.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,27 +1,128 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	_ "embed"
+	"errors"
 	"flag"
-	"os"
-	"os/signal"
-	"syscall"
+	"fmt"
+	"html/template"
+	"io"
+	"log/slog"
+	"net/http"
+	"strings"
+	"time"
 
-	"github.com/google/subcommands"
-	"go.seankhliao.com/vanity/subcommands/serve"
+	"go.seankhliao.com/svcrunner/v3/framework"
+	"go.seankhliao.com/svcrunner/v3/observability"
+	"go.seankhliao.com/webstyle"
+	"go.seankhliao.com/webstyle/webstatic"
+)
+
+var (
+	//go:embed index.md
+	indexRaw []byte
+
+	//go:embed repo.md.tpl
+	repoRaw string
+	repoTpl = template.Must(template.New("").Parse(repoRaw))
+
+	headRaw = `
+    <meta
+      name="go-import"
+      content="go.seankhliao.com/{{ .Repo }} git https://{{ .Source }}/{{ .Repo }}">
+    <meta
+      name="go-source"
+      content="{{ .Host }}/{{ .Repo }}
+        https://{{ .Source }}/{{ .Repo }}
+        https://{{ .Source }}/{{ .Repo }}/tree/master{/dir}
+        https://{{ .Source }}/{{ .Repo }}/blob/master{/dir}/{file}#L{line}">
+`
+	headTpl = template.Must(template.New("").Parse(headRaw))
 )
 
 func main() {
-	name := "earbug"
-	fset := flag.NewFlagSet(name, flag.ExitOnError)
-	cmdr := subcommands.NewCommander(fset, name)
-	cmdr.Register(&serve.Cmd{}, "server")
+	var host, source string
+	framework.Run(framework.Config{
+		RegisterFlags: func(fset *flag.FlagSet) {
+			fset.StringVar(&host, "vanity.host", "go.seankhliao.com", "host this server runs on")
+			fset.StringVar(&source, "vanity.source", "github.com/seankhliao", "where the code is hosted")
+		},
+		Start: func(ctx context.Context, o *observability.O, m *http.ServeMux) (func(), error) {
+			o = o.Component("vanity")
 
-	cmdr.Register(cmdr.HelpCommand(), "other")
+			render := webstyle.NewRenderer(webstyle.TemplateCompact)
+			index, err := render.RenderBytes(indexRaw, webstyle.Data{})
+			if err != nil {
+				return nil, fmt.Errorf("render index template: %w", err)
+			}
+			t0 := time.Now()
 
-	fset.Parse(os.Args[1:])
+			webstatic.Register(m)
+			m.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+				ctx, span := o.T.Start(r.Context(), "serve vanity")
+				defer span.End()
 
-	ctx := context.Background()
-	ctx, _ = signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
-	os.Exit(int(cmdr.Execute(ctx)))
+				slogHTTPRequest := slog.Group("http_request",
+					slog.String("method", r.Method),
+					slog.String("url", r.URL.String()),
+					slog.String("proto", r.Proto),
+					slog.String("user_agent", r.UserAgent()),
+					slog.String("remote_address", r.RemoteAddr),
+					slog.String("referrer", r.Referer()),
+					slog.String("x-forwarded-for", r.Header.Get("x-forwarded-for")),
+					slog.String("forwarded", r.Header.Get("forwarded")),
+				)
+
+				if r.Method != http.MethodGet {
+					o.HTTPErr(ctx, "GET only", errors.New("method not allowed"), rw, http.StatusMethodNotAllowed, slogHTTPRequest)
+					return
+				}
+
+				p := strings.TrimPrefix(r.URL.Path, "/")
+				if p == "" { // index
+					http.ServeContent(rw, r, "index.html", t0, bytes.NewReader(index))
+					o.L.LogAttrs(ctx, slog.LevelInfo, "served index page",
+						slogHTTPRequest,
+					)
+					return
+				}
+
+				// other pages
+				repo, _, _ := strings.Cut(p, "/")
+				data := map[string]string{"Repo": repo, "Source": source, "Host": host}
+
+				var buf1 bytes.Buffer
+				err := repoTpl.Execute(&buf1, data)
+				if err != nil {
+					o.HTTPErr(ctx, "render repo", err, rw, http.StatusInternalServerError)
+					return
+				}
+				var buf2 bytes.Buffer
+				err = headTpl.Execute(&buf2, data)
+				if err != nil {
+					o.HTTPErr(ctx, "render head", err, rw, http.StatusInternalServerError)
+					return
+				}
+
+				var buf3 bytes.Buffer
+				err = render.Render(&buf3, &buf1, webstyle.Data{
+					Head: buf2.String(),
+				})
+				if err != nil {
+					o.HTTPErr(ctx, "render html", err, rw, http.StatusInternalServerError)
+					return
+				}
+				_, err = io.Copy(rw, &buf3)
+				if err != nil {
+					o.HTTPErr(ctx, "write response", err, rw, http.StatusInternalServerError)
+					return
+				}
+
+				o.L.LogAttrs(ctx, slog.LevelInfo, "served module page", slog.String("repo", repo), slogHTTPRequest)
+			})
+			return nil, nil
+		},
+	})
 }
